b7e61904a5b8094284f3149a1cf826690414f522
Taylor Behrens <taylorbehrens@gmail.com>
2024-01-24 18:01:45 -0800

    ...

--- recipefinder/src/App.js
+++ recipefinder/src/App.js
@@ -1,10 +1,18 @@
+import { useState, useCallback, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 const api_url = "https://api.spoonacular.com/recipes/findByIngredients";
 const api_key = "b092758163cf40dbaea8e7b5e9c5c7ff";
 
+
+
 function App() {
+
+const fetchApi = GET => useEffect{
+
+}
+
   return (
     <div className="App">
       <header className="App-header">
