06485e6294a4e8f8924bf5a2a439dbd0622042b0
Taylor Behrens <taylorbehrens@gmail.com>
2024-02-22 19:45:10 -0800

    Build prep

--- recipefinder/src/App.js
+++ recipefinder/src/App.js
@@ -2,10 +2,27 @@ import { useState, useCallback, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const api_url = "https://api.spoonacular.com/recipes/findByIngredients";
+const api_url = "https://api.spoonacular.com/recipes/findByIngredients"; 
 const api_key = "b092758163cf40dbaea8e7b5e9c5c7ff";
 
 // Due date - Feb 25 ------ March 4
+
+// useEffect(() => {
+//   // Function to fetch recipes based on ingredients
+//   const fetchRecipes = async () => {
+//     try {
+//       // Replace 'YOUR_API_KEY' with the actual API key from the recipe API
+//       const apiKey = 'YOUR_API_KEY';
+//       const response = await axios.get(
+//         `https://api.example.com/recipes?ingredients=${ingredients.join(',')}&apiKey=${apiKey}`
+//       );
+
+//       // Update the recipes state with the fetched data
+//       setRecipes(response.data);
+//     } catch (error) {
+//       console.error('Error fetching recipes:', error);
+//     }
+//   };
 
 function App() {
 
